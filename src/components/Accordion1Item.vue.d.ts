3b0035ab555eb277455952e00732ac3a86ca12d3
Branislav Cepilek <branislav.cepilek@gmail.com>
2024-06-11 15:21:10 +0200

    Remove most of *.d.ts files

--- src/components/Accordion1Item.vue.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: import("vue").DefineComponent<{}, {}, {
-    activeAccordion: number | null;
-}, {}, {
-    toggleAccordion(id: number): void;
-}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<{}>>, {}, {}>;
-export default _default;
